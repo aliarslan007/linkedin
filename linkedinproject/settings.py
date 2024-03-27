6dfa17ba9dd71eb45b0664ada45d8b10f6b40d2b
aliarslan007 <aliarslanofficial@gmail.com>
2024-03-27 23:03:42 +0500

    allowed hosts

--- linkedinproject/settings.py
+++ linkedinproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9z$qze!65n)ao^&+#arrb&p#ts7df5pl6pg%l*5a7+f1b#cn2#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
